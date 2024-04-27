3d24e85e25c9e2b0311bd66645611368f195e3d9
stoyanoronaldo <stoyanoronaldo@abv.bg>
2024-04-27 15:00:42 +0300

    fixing streamlit_app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import os
 import streamlit as st
-from functions import *
+from openai import OpenAI
 
+client = OpenAI(
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
 
 st.title("Text to KG")
 
@@ -9,9 +12,19 @@ user_text = st.text_input("Enter some text:")
 question =  question = "For the givem text provide all concepts and relations between them. \
                 Present the result in turtle format using Rdfs schema, schema.org and example.org for the enteties.\nText: "                
 
+
+
 if st.button("Get Answer"):
     if user_text:
-        answer = chat_with_chatgpt(question + user_text)
-        st.write("Answer:", answer)
+        chat_completion = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": question + user_text,
+                }
+            ],
+            model="gpt-3.5-turbo",
+        )
+        st.write("Answer:", chat_completion)
     else:
         st.write("Please enter some text.")
