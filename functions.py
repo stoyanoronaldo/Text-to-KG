b6d22f22a6d76d088655b9d705bb66aa5790ed4f
stoyanoronaldo <stoyanoronaldo@abv.bg>
2024-05-05 17:25:22 +0300

    added validation to ui

--- functions.py
+++ functions.py
@@ -1,5 +1,16 @@
-from rdflib import Graph
+from rdflib import Graph, URIRef
 from streamlit_agraph import Node, Edge, TripleStore
+import re
+import asyncio
+
+def get_or_create_event_loop():
+    try:
+        return asyncio.get_event_loop()
+    except RuntimeError as ex:
+        if "There is no current event loop in thread" in str(ex):
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+            return loop
 
 def get_answer_from_file(file_name):
     with open(file_name, 'r') as file:
@@ -24,6 +35,49 @@ def save_answer_to_file(answer):
     with open(file_path, 'w') as file:
         file.write(answer)
 
+def is_valid_turtle(ttl_string):
+    g = Graph()
+
+    try:
+        g.parse(data=ttl_string, format="ttl")
+        return True, "Valid Turtle"
+    except Exception as e:
+        return False, f"An error occurred: {e}"
+
+def fix_turtle_syntax(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        content = file.read()
+
+    # Fix common Turtle syntax issues
+    # Ensure proper spacing around semicolons and periods
+    #content = re.sub(r'\s*;\s*', ' ; ', content)
+    #content = re.sub(r'\s*\.\s*', ' .\n', content)
+    content = re.sub(r'\s*\:\s*', ':', content)
+
+    # Ensure quotes are properly closed
+    #content = re.sub(r'\"([^\"]*)\"', r'"\1"', content)
+
+    # Write the fixed content back to the file
+    with open(file_path, 'w', encoding='utf-8') as file:
+        file.write(content)
+
+    print(f"Fixed syntax in {file_path}")
+
+def fix_uris(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        content = file.read()
+
+    # Remove line breaks within URIs
+    content = re.sub(r'http://www \.\n\s*w3 \.\n\s*org/', 'http://www.w3.org/', content)
+    content = re.sub(r'http://schema \.\n\s*org/', 'http://schema.org/', content)
+    content = re.sub(r'http://example \.\n\s*org/', 'http://example.org/', content)
+
+    # Write the fixed content back to the file
+    with open(file_path, 'w', encoding='utf-8') as file:
+        file.write(content)
+
+    print(f"URIs fixed in {file_path}")
+
 def build_graph(ttl_string):
     g = Graph()
     g.parse(data=ttl_string, format="ttl")
@@ -34,13 +88,19 @@ def build_graph(ttl_string):
     edges = []
 
 
-    for s, p, o in g:
-        if str(s) not in made_nodes:
-            nodes.append(Node(id=str(s), size=20, title=str(s)))
-            made_nodes.add(str(s))
-        if str(o) not in made_nodes:
-            nodes.append(Node(id=str(o), size=20, title=str(o)))
-            made_nodes.add(str(o))
-        edges.append(Edge(source=str(s), target=str(o), type="CURVE_SMOOTH", title=str(p), width=3))
+    for subject, predicate, object in g:
+        if str(subject) not in made_nodes:
+            nodes.append(Node(id=str(subject), size=20, label=str(subject).split("/")[-1], title=str(subject)))
+            made_nodes.add(str(subject))
+        if isinstance(object, URIRef) and str(object) not in made_nodes:
+            nodes.append(Node(id=str(object), size=20, label=str(object).split("/")[-1], title=str(object)))
+            made_nodes.add(str(object))
+        if isinstance(object, URIRef):
+            edges.append(Edge(source=str(subject), target=str(object), arrowStrikethrough=False ,title=str(predicate).split("/")[-1], width=3))
+        else:
+            for node in nodes:
+                if node.id == str(subject):
+                    node.title = node.title + "\n\n" + str(predicate) + "\n" + str(object).split("/")[-1]
+                    break
 
     return nodes, edges
--- streamlit_app.py
+++ streamlit_app.py
@@ -3,16 +3,6 @@ from functions import *
 import streamlit as st
 import streamlit_agraph
 from streamlit_agraph import Config, ConfigBuilder
-import asyncio
-
-def get_or_create_event_loop():
-    try:
-        return asyncio.get_event_loop()
-    except RuntimeError as ex:
-        if "There is no current event loop in thread" in str(ex):
-            loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(loop)
-            return loop
 
 # Ensure an event loop is created before importing poe_api_wrapper
 loop = get_or_create_event_loop()
@@ -27,9 +17,36 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded"
 )
-def page1():
 
-    user_text = st.text_area(label=" ", placeholder="Enter some text",height=150)
+if 'page_num' not in st.session_state:
+    st.session_state.page_num = 1
+
+page_options = ["Graph generation", "Graph view"]
+page_selection = st.sidebar.radio(" ", page_options)
+
+if page_selection == "Graph generation":
+    st.session_state.page_num = 1
+elif page_selection == "Graph view":
+    st.session_state.page_num = 2
+
+if 'answer' not in st.session_state:
+    st.session_state.answer = None
+
+if 'user_text' not in st.session_state:
+    st.session_state.user_text = None
+
+if "get_answer" not in st.session_state:
+    st.session_state.get_answer = False
+
+if "validate_turtle" not in st.session_state:
+    st.session_state.validate_turtle = False
+
+if "is_valid_turtle" not in st.session_state:
+    st.session_state.is_valid_turtle = False
+
+if st.session_state.page_num == 1:
+
+    user_text = st.text_area(label=" ", placeholder="Enter some text", height=150, value=st.session_state.user_text or "")
 
     comment = """tokens = {
             'b': os.environ.get("POE_B"),
@@ -44,40 +61,59 @@ def page1():
     task = '''For the given text provide all concepts and relations between them in turtle format using \
             Rdfs schema, schema.org and example.org for the enteties.\nText: '''
     question = task + user_text"""
+    col1, col2= st.columns([0.15, 0.85])
 
-    get_answer_buuton = st.button("Get Answer")
+    with col1:
+        get_answer_btn = st.button("Get Answer")
 
-    if get_answer_buuton:
+    if get_answer_btn:
         if user_text:
-            comment = """for chunk in client.send_message(bot, question):
-                pass
-            save_answer_to_file(chunk["text"])
-            answer = get_answer_from_string(chunk["text"])"""
-            answer = get_answer_from_file('response.txt')
-            st.code(answer, language="turtle")
+            st.session_state.user_text = user_text
+            st.session_state.get_answer = True
         else:
-            st.write("Please enter some text.")
+            st.write("<font color='red'>Please enter some text.</font>", unsafe_allow_html=True)
+            st.session_state.get_answer = False
 
-def page2():
-    answer = get_answer_from_file('response.txt')
-    config = Config(height=550,
-		        width=1200, 
-                nodeHighlightBehavior=True,
-                highlightColor="#F7A7A6", 
-                directed=True,
-                physics=False, 
-                hierarchical=False, 
-                collapsible=True)
+    with col2:
+        if st.session_state.get_answer and (not st.session_state.validate_turtle):
+            if st.button("Validate Turtle"):
+                st.session_state.validate_turtle = True
+    
+    if st.session_state.validate_turtle:
+        comment = """for chunk in client.send_message(bot, question):
+                    pass
+                save_answer_to_file(chunk["text"])
+                answer = get_answer_from_string(chunk["text"])"""
+        answer = get_answer_from_file('response.txt')
+    
+        is_valid_ttl, is_valid_string = is_valid_turtle(answer)
 
-    nodes, edges = build_graph(answer)
+        if is_valid_ttl:
+            st.session_state.is_valid_turtle = True
+            st.write(f"<font color='green'>{is_valid_string}</font>", unsafe_allow_html=True)
+            with st.expander("View turtle"):
+                st.code(answer, language="turtle")
+            st.session_state.answer = answer
+        else:
+            st.write(f"<font color='red'>{is_valid_string}</font>", unsafe_allow_html=True)
 
-    streamlit_agraph.agraph(nodes=nodes, edges=edges, config=config)
+        st.session_state.validate_turtle = False
+        st.session_state.get_answer = False
 
+elif st.session_state.page_num == 2:
+    if st.session_state.is_valid_turtle:
+        answer = st.session_state.answer
+        config = Config(height=550,
+                    width=1200,
+                    nodeHighlightBehavior=True,
+                    highlightColor="#F7A7A6", 
+                    directed=True,
+                    physics=False, 
+                    hierarchical=False, 
+                    collapsible=True)
 
-page_options = ["Graph generation", "Graph view"]
-page_selection = st.sidebar.radio(" ", page_options)
+        nodes, edges = build_graph(answer)
 
-if page_selection == "Graph generation":
-    page1()
-elif page_selection == "Graph view":
-    page2()
+        streamlit_agraph.agraph(nodes=nodes, edges=edges, config=config)
+    else:
+        st.write("<div style='font-size: 50px; color: red;'>You don't have a valid turtle to build the graph.</div>", unsafe_allow_html=True)
